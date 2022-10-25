d80c03c57bfc877542401b0fe7973925687000b2
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-10-25 07:55:23 +0600

    This is the commti

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div >
             <hr /><hr /><hr />
 
             <h1>Footer</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quo rem quasi? Aliquam quo molestiae, ad velit eum amet quidem quam nobis ut, nam odit suscipit maxime soluta enim distinctio!</p>
-        </div>
+
+
+        </div >
     );
 };
 
