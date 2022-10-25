c18d0d8d003a0683f5c3e30ede8d3ed1de95b4ca
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-10-25 09:59:39 +0600

    This isthe nex

--- src/Pages/SideNav/SideNav.jsx
+++ src/Pages/SideNav/SideNav.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 
 const SideNav = () => {
 
@@ -10,8 +15,40 @@ const SideNav = () => {
     console.log(data);
     return (
         <div>
-            <h1>SideNav</h1>
-            <h1>SideNav</h1>
+            <ButtonGroup vertical>
+                <Button>Button</Button>
+                <Button>Button</Button>
+
+                <DropdownButton
+                    as={ButtonGroup}
+                    title="Dropdown"
+                    id="bg-vertical-dropdown-1"
+                >
+                    <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
+                </DropdownButton>
+
+                <Button>Button</Button>
+                <Button>Button</Button>
+
+                <DropdownButton
+                    as={ButtonGroup}
+                    title="Dropdown"
+                    id="bg-vertical-dropdown-2"
+                >
+                    <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
+                </DropdownButton>
+
+                <DropdownButton
+                    as={ButtonGroup}
+                    title="Dropdown"
+                    id="bg-vertical-dropdown-3"
+                >
+                    <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
+                </DropdownButton>
+            </ButtonGroup>
         </div>
     );
 };
